cfcbe811f130d26871c55759bd94e3b5d516b4a1
rupajsoni <rsoni@my.harrisburgu.edu>
2022-01-03 15:38:00 -0500

    Add argument validation

--- src/main/java/com/gsr/OrderBookApp.java
+++ src/main/java/com/gsr/OrderBookApp.java
@@ -7,8 +7,13 @@ public class OrderBookApp {
         Runtime.getRuntime().addShutdownHook(new ShutDownHook());
 
         OrderBookExecutor orderBookExecutor = new OrderBookExecutor();
-        String productId = args[0];
 
+        if(args == null || args.length == 0){
+            System.out.println("Please pass product ID argument. Refer https://api.exchange.coinbase.com/products for supported products");
+            System.exit(1);
+        }
+
+        String productId = args[0];
         orderBookExecutor.startApplication(productId);
     }
 }
